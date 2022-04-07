53218cb58ff173e44a48828cd1b6e71874ec3547
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-07 18:53:24 -0300

    Melhorias no eslint

--- src/app/main/books/forms/forms.component.ts
+++ src/app/main/books/forms/forms.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent implements OnInit {
+export class FormsComponent {
+  bookForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      authorName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
+    });
   }
-
 }
