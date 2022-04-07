87a4ee6b34b38ea29ef983d8d763dbf31fd0d236
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-07 18:08:36 -0300

    Formulário de criação de um livro

--- src/app/main/books/books.module.ts
+++ src/app/main/books/books.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { SharedModule } from "src/app/shared/shared.module";
 import { BooksComponent } from "./books.component";
 import { BooksRoutes } from "./books.routing";
+import { FormsComponent } from "./forms/forms.component";
 
 @NgModule({
-  declarations: [BooksComponent],
-  imports: [CommonModule, BooksRoutes],
+  declarations: [BooksComponent, FormsComponent],
+  imports: [CommonModule, BooksRoutes, SharedModule],
 })
 export class BooksModule {}
--- src/app/main/books/books.routing.ts
+++ src/app/main/books/books.routing.ts
@@ -1,6 +1,10 @@
 import { RouterModule, Routes } from "@angular/router";
 import { BooksComponent } from "./books.component";
+import { FormsComponent } from "./forms/forms.component";
 
-const routes: Routes = [{ path: "", component: BooksComponent }];
+const routes: Routes = [
+  { path: "", component: BooksComponent },
+  { path: "new", component: FormsComponent },
+];
 
 export const BooksRoutes = RouterModule.forChild(routes);
