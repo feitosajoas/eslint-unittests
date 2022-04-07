1926ea0e5304ce167c88cd16b04991da2cb4cc56
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-07 19:31:54 -0300

    Melhorias no componente de input

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,55 @@
-import { Component, Input } from "@angular/core";
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+const INPUT_FIELD_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputComponent),
+  multi: true
+};
 @Component({
-  selector: "app-input",
-  templateUrl: "./input.component.html",
-  styleUrls: ["./input.component.scss"],
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [INPUT_FIELD_VALUE_ACCESSOR]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() isInput: boolean = true;
-  @Input() inputType: string = "";
-  @Input() inputId: string = "";
-  @Input() placeholder: string = "";
-  @Input() labelName: string = "";
+  @Input() inputType: string = '';
+  @Input() inputId: string = '';
+  @Input() placeholder: string = '';
+  @Input() labelName: string = '';
+  @Input() isReadOnly = false;
+  @Input() control: any;
+
+  private innerValue: any;
+
+  get value() {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCb(v);
+    }
+  }
+
+  onChangeCb: (_: any) => void = () => {};
+  onTouchedCb: (_: any) => void = () => {};
+
+  writeValue(v: any): void {
+    this.value = v;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCb = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCb = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isReadOnly = isDisabled;
+  }
 }
