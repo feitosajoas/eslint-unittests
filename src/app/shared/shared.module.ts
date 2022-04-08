5cab5d7452a97ee5bd3d87bfbf335bc077de1485
feitosajoas <joasfeitosadelimasilva@gmail.com>
2022-04-08 16:34:10 -0300

    Criado errorMessageComponent, FormDebugComponent e importando FormsModule para usar [(ngModel)]

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { InputComponent } from "./input/input.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ErrorMessageComponent } from './error-message/error-message.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [InputComponent],
-  imports: [CommonModule],
-  exports: [InputComponent],
+  declarations: [InputComponent, ErrorMessageComponent, FormDebugComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [InputComponent, FormDebugComponent]
 })
 export class SharedModule {}
